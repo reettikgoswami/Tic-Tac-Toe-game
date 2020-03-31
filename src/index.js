327721a701871e9f12442e70e648ab90ea50dcc2
reettik <reettikgoswami1997@gmail.com>
2020-03-31 19:22:33 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -19,7 +18,7 @@ class Board extends React.Component {
         onClick={() => this.props.onClick(i)}
       />
     );
-  }
+}
 
   render() {
     return (
@@ -125,7 +124,6 @@ class Game extends React.Component {
 }
 
 // ========================================
-
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
